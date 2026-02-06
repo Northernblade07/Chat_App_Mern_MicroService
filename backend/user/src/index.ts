f7344bf9d9f80f40dcb04b7f0d5c8844380bbc7c
Prashannt Bhandari <prashantbhandaricr07@gmail.com>
2026-02-06 20:30:42 +0530

    implement redis for caching in user service

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './config/db.js';
+import { createClient } from 'redis';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+export const redisClient = createClient({
+    url:process.env.REDIS_URL!,
+});
+
+redisClient.connect().then(()=>console.log("connected to redis")). catch(console.error);
 
 const startServer = async () => {
   try {
